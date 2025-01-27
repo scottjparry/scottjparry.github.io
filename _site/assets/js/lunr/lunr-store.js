12dd9f28fef655433c187986b01450c21d06ddf9
Scott Parry <sopa@4bbl0m.onmicrosoft.com>
2025-01-27 18:49:06 +1100

    Update of about page

--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -1,6 +1,6 @@
 var store = [{
         "title": "So long and thanks for all the...cuttlefish?!",
-        "excerpt":"heading   Test 1234   Welcome to My First Post   This is an example post. You can add content in Markdown or HTML. Write about your ideas, projects, or anything you want to share.  ","categories": ["blog"],
+        "excerpt":"Welcome to My First Post   This is an example post. You can add content in Markdown or HTML. Write about your ideas, projects, or anything you want to share.  ","categories": ["blog"],
         "tags": ["introduction","blogging"],
         "url": "/blog/Test2/",
         "teaser": null
